0e5687e561964bd1e4b2c4073e7c07b4b34189b2
Lyndsie Lane <lyndsielane@yahoo.com>
2021-07-23 18:39:15 -0400

    added hamburger menu for mobile response

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -15,3 +15,10 @@ $(document).ready(function() {
       }
    });
  });
+
+ const hamburger = document.getElementById('hamburger')
+ const navUl = document.getElementById('nav-ul')
+
+ hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+ });
