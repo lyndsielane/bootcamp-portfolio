3bd093acbc00d3d413d2180404184b97510e49c0
Lyndsie Lane <lyndsielane@yahoo.com>
2021-09-23 17:32:01 -0400

    updated top resume link

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,26 +1,29 @@
-$(document).ready(function() {
-  $(window).on('scroll', function() {
-    if($(window).scrollTop() < 1000) {
-      $('.hero').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
-      $('.hero h1').css('top', 50 + ($(window).scrollTop() * .1) + '%');
-      $('.hero h1').css('opacity', 1 - ($(window).scrollTop() * .003));
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() < 1000) {
+      $(".hero").css(
+        "background-size",
+        130 + parseInt($(window).scrollTop() / 5) + "%"
+      );
+      $(".hero h1").css("top", 50 + $(window).scrollTop() * 0.1 + "%");
+      $(".hero h1").css("opacity", 1 - $(window).scrollTop() * 0.003);
     }
-    
-    if($(window).scrollTop() >= $('.content-wrapper').offset().top - 300) {
-      $('.nav-bg').removeClass('bg-hidden');
-      $('.nav-bg').addClass('bg-visible');
+
+    if ($(window).scrollTop() >= $(".content-wrapper").offset().top - 300) {
+      $(".nav-bg").removeClass("bg-hidden");
+      $(".nav-bg").addClass("bg-visible");
     } else {
-      $('.nav-bg').removeClass('bg-visible');
-      $('.nav-bg').addClass('bg-hidden');
+      $(".nav-bg").removeClass("bg-visible");
+      $(".nav-bg").addClass("bg-hidden");
     }
   });
 });
 
-const hamburger = document.getElementById('hamburger')
-const navUl = document.getElementById('nav-ul')
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("nav-ul");
 
-hamburger.addEventListener('click', () => {
-  navUl.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+  navUl.classList.toggle("show");
 });
 
 colors = {
@@ -37,28 +40,29 @@ colors = {
   grunt: "#F4A419",
   npm: "#C53635",
   gem: "#671110",
-  'github-square': 'black',
+  "github-square": "black",
 };
-  
-document.querySelectorAll(".skillicons li i").forEach((item,index) => {
-  item.style.color = colors[item.className.substr(7, item.className.length)]})
-                            
-document.addEventListener("click",() => {
+
+document.querySelectorAll(".skillicons li i").forEach((item, index) => {
+  item.style.color = colors[item.className.substr(7, item.className.length)];
+});
+
+document.addEventListener("click", () => {
   animateSkills = anime({
-  targets: ".skillicons li",
-  translateY: [70, 0],
-  opacity: [0, 1],
-  autoplay: true,
-  duration: 1500,
-  delay: anime.stagger(600)
-  })
-})
-  
-document.querySelectorAll(".skillicons li i").forEach((item,index) => {
-  (function(counter) {
-    setTimeOut(function() {
-      item.style.color = colors[item.className.substr(7, item.className.length)]
-    },
-    100*counter);
+    targets: ".skillicons li",
+    translateY: [70, 0],
+    opacity: [0, 1],
+    autoplay: true,
+    duration: 1500,
+    delay: anime.stagger(600),
+  });
+});
+
+document.querySelectorAll(".skillicons li i").forEach((item, index) => {
+  (function (counter) {
+    setTimeout(function () {
+      item.style.color =
+        colors[item.className.substr(7, item.className.length)];
+    }, counter);
   })(index);
 });
